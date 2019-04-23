1db28ab14d5bd6b8d6ca439c36496f13447249d9
Daniel Everett <deverett@gmail.com>
2019-04-24 06:59:09 +1000

    javaws WIP

---
+++ javaws/entrypoint.sh
@@ -0,0 +1,14 @@
+./autogen.sh
+./configure \
+	--with-itw-libs=BUNDLED \
+	--without-firefox \
+	--without-chromium \
+	--without-opera \
+	--without-midori \
+	--without-epiphany \
+	--without-browser-tests \
+	--with-ecj \
+	--with-jdk-home=/usr/lib/jvm/java-8-openjdk-amd64/jre \
+	--with-jre-home=/usr/lib/jvm/java-8-openjdk-amd64/jre 
+
+
